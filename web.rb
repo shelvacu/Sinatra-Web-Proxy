e12120ef0fdd5b1eb4ec3c8750c18e6ebbe04fc7
Shelvacu Vevevende <shelvacu@gmail.com>
2013-05-07 01:55:00 -0700

    init commit; might work first try :D

---
+++ web.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'open-uri'
+
+class SiteReader
+  def initialize(url)
+    @siteIO = open(url)
+  end
+
+  def each
+    @siteIO.gets("\n",65535)
+  end
+end
+
+get('*'){
+  SiteReader.new(request.url)
+}
+
