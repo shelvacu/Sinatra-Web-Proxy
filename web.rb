12d79b743acad79b7f966ae57497fdd8e0148548
Shelvacu Vevevende <shelvacu@gmail.com>
2013-05-07 02:20:30 -0700

    fuck commit messages.

--- web.rb
+++ web.rb
@@ -1,6 +1,22 @@
-require 'sinatra'
+#require 'sinatra'
 require 'open-uri'
+require 'gserver'
 
+class LogServ < GServer
+  def initialize(port=ARGV[1],*args)
+    super port,*args
+  end
+
+  def serve(io)
+    $stdout.puts io.gets
+  end
+end
+
+serv = LogServ.new
+serv.start
+serv.join
+
+=begin
 class SiteReader
   def initialize(url)
     @siteIO = open(url)
@@ -15,3 +31,4 @@ get('/'){"wrkz"}
 get('*'){
   SiteReader.new(request.url)
 }
+=end
