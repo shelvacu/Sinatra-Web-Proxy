b7290ec966671aeaa65c7d7b70955a71f6f2be69
Shelvacu Vevevende <shelvacu@gmail.com>
2013-05-15 01:30:50 -0700

    STUFF. IT F***ING WORKS PROPER NOW (except POST and a few other oddities)

--- lib/mongler.rb
+++ lib/mongler.rb
@@ -4,15 +4,24 @@ class InvalidURL < StandardError
 end
 
 class Mongler
-  def initialize(url,html = true)#,prefix=nil)
+  def initialize(url,page_params)#,prefix=nil)
     url = "http://#{url}" if !is_url?(url)
     raise InvaildURL if !is_url?(url)
-    @url = URI.parse(url)
+    @uri = URI.parse(url)
+    @uri.query ||= ""
+    @uri.query += URI.encode_www_form(page_params)
+    #RACK_ENV["processing_url"] = @uri
     #@prefix = prefix
-    open(url, "User-Agent" => "Mongler Ruby/#{RUBY_VERSION}") do |f|
-      @content_type = f.content_type
-      @charset = f.charset
-      @raw_page = f.read
+    begin
+      open(@uri.to_s, "User-Agent" => "Mongler Ruby/#{RUBY_VERSION}") do |f|
+        @content_type = f.content_type
+        @charset = f.charset
+        @raw_page = f.read
+      end
+    rescue OpenURI::HTTPError
+      @content_type = "text/plain"
+      @charset = "utf-8"
+      @raw_page = "teh other server spit out a 404 for url #{@uri.to_s}"
     end
     @doc = nil
     begin
@@ -28,9 +37,9 @@ class Mongler
   attr_reader :content_type,:charset
 
   def proxy_uri(page)
-    full_goto_url = @url.merge(page)
-    prox_uri = URI("/proxy")
-    prox_uri.query = URI.encode_www_form("url" => full_goto_url.to_s)
+    full_goto_url = @uri.merge(page)
+    prox_uri = URI("/proxy/#{URI.encode_www_form_component full_goto_url.to_s}/")
+    #prox_uri.query = URI.encode_www_form("url" => full_goto_url.to_s)
     return prox_uri
   end
 
@@ -39,7 +48,7 @@ class Mongler
   end
 
   def is_url?(url=nil)
-    url.nil? ? !@url.scheme.nil? : !(URI.parse(url).scheme.nil?)
+    url.nil? ? !@uri.scheme.nil? : !(URI.parse(url).scheme.nil?)
   end
 
   def mangle(tag, attribute)
--- main.rb
+++ main.rb
@@ -2,9 +2,14 @@ require 'sinatra'
 require 'haml'
 require 'sass'
 require 'open-uri'
+require 'pp'
 require './lib/mongler'
 
 set :haml, :format=>:html5
+
+error 404 do
+  "You didn't put in a url! How could you!(this may be a bug in the code)"
+end
 
 get '/' do
   haml :index
@@ -15,7 +20,17 @@ get('/main.css'){sass :main}
 get('/proxy.css'){sass :proxy}
 
 get '/proxy' do
-  res = mangle_page params[:url]
+  error 404 if params[:url].nil?
+  redirect "/proxy/#{URI.encode_www_form_component params[:url]}/"
+end
+
+get '/proxy/*/*' do
+  error 404 if params[:splat].nil?
+  params[:splat][1] ||= ""
+  url = params[:splat].first
+  page_params = params
+  page_params.delete(:splat)
+  res = mangle_page url+(page_params.empty? ? '' : '?'+URI.encode_www_form(page_params)),{}
   if res[:charset].nil?
     content_type res[:content_type]
   else
@@ -29,8 +44,8 @@ get '/proxy' do
   end
 end
 
-def mangle_page(url)
-  doc = Mongler.new(url)
+def mangle_page(url,p)
+  doc = Mongler.new(url,p)
   doc.mangle('img', 'src')
   doc.mangle('link', 'href')
   doc.mangle('a', 'href')
